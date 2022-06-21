c6fd50ccea883fbe9b34f2bd51246af202dca5ec
DimaGaleev <107946130+DimaGaleev@users.noreply.github.com>
2022-06-22 02:14:05 +0700

    Add files via upload

--- js/jquery.main.js
+++ js/jquery.main.js
@@ -12,31 +12,8 @@ $(document).ready(function() {
 		} else {
 			// everything looks good!
 			event.preventDefault();
-			submitForm();
 		}
 	});
-
-
-	function submitForm(){
-		// Initiate Variables With Form Content
-		var name = $("#name").val();
-		var email = $("#email").val();
-		var phone = $("#phone").val();
-
-		$.ajax({
-			type: "POST",
-			url: "php/form-process.php",
-			data: "name=" + name + "&email=" + email + "&phone=" + phone,
-			success : function(text){
-				if (text == "success"){
-					formSuccess();
-				} else {
-					formError();
-					submitMSG(false,text);
-				}
-			}
-		});
-	}
 
 	function formSuccess(){
 		$("#contactForm")[0].reset();
